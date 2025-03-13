d731533d995b72050d36fa3fa20ca75dd154275e
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-03-13 11:43:51 +0700

    prosedur untuk input

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 using namespace std;
+int arr[20];        //deklarasi variable global array dengan ukuran 20
+int n;              //deklarasi variable global n untuk menyimpan banyak element pada array
 void  input(){          
     while(true){
         cout << "Masukan banyak elemenen maksimal 20 elemen\n" << endl;
@@ -10,5 +12,15 @@ void  input(){
         else{
             cout  << "\nArray dapat mempunyai maksimal 20 elemen\n" << endl;
         }
+        cout << endl;
+        cout << "====================" << endl;
+        cout << "Masukan elemen array" << endl;
+        cout << "====================" << endl;
+
+    for (int i = 0; i <n; i++) {
+    cout << "Data ke-" << (i+1) << ";")
+    cin >> arr[i];
     }
-}
+ }
+
+    }
